e96853b123a78099ab1e8db9198d90275a67ccd6
NhanNT <iits.williamnhan@gmail.com>
2021-07-19 17:06:42 -0500

    add network security group, rule, associate network interface to security group.

--- azurerm/main.tf
+++ azurerm/main.tf
@@ -46,3 +46,28 @@ resource "azurerm_public_ip" "web_server_public_ip" {
   resource_group_name = azurerm_resource_group.web_server_rg.name
   allocation_method = var.enviroment == "production" ? "Static" : "Dynamic"
 }
+
+resource "azurerm_network_security_group" "web_server_nsg" {
+  name = "${var.resource_prefix}-nsg"
+  location = var.web_server_location
+  resource_group_name = azurerm_resource_group.web_server_rg.name
+}
+
+resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
+  name = "RDP Inbound"
+  priority = 100
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "Tcp"
+  source_port_range = "*"
+  destination_port_range = "3389"
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+  resource_group_name = azurerm_resource_group.web_server_rg.name
+  network_security_group_name = azurerm_network_security_group.web_server_nsg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "web_server_nsg_association" {
+  network_security_group_id = azurerm_network_security_group.web_server_nsg.id
+  network_interface_id = azurerm_network_interface.web_server_nic.id
+}
